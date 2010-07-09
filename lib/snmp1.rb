db43fc8580e9a7638d98b0b54d882ce96b5a95f4
Scott Shanks <Scott.Shanks@emerson.com>
2010-07-09 16:55:42 -0400

    Created snmp_walk method

--- lib/snmp1.rb
+++ lib/snmp1.rb
@@ -33,8 +33,30 @@ module  Snmp
 
   #set SNMP value through system command
   def snmp_set(ip,com_str,mib,oid,type,val)
-    command = 'snmpset -v2c -c'<< com_str << ' ' << ip << ' ' << mib << '::' << oid <<' '<<type <<' '<< val
-    puts "set command = #{command}"
+  end
+
+  #Performs an snmpwalk, parses each line of output and returns as an array in
+  #the format: OID, Value, Type, OID, Value, Type,...
+  def snmp_walk(ip=@test_site,com_str=@community_string,version='2c',oid='.1')
+    command = "snmpwalk -v #{version} -c #{com_str} #{ip} #{oid}"
+    snmp_data = `#{command}`.to_s
+    
+    a = Array.new
+    snmp_data.each_line do |line|
+      if line =~ /No more variables/ then next; end;
+      a << line.split('=')[0].strip
+      line.split('=')[1].each do |value|
+        a << value.split(': ')[1].strip.each do |str| #This should be the value
+          if str.match(/\d{4}/) or str.match(/\w\.\d*/) #TODO - Cleanup certain values in the walk...
+            a << str.split('::')[1]
+          else
+            a << str.split('::')[1]
+          end
+        end
+        a << value.split(':')[0].strip  #This should be the type (e.g STRING)
+        end
+      end
+    return a
   end
 
 end
